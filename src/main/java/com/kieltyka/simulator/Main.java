0474deceae69e375a2c90c1cfc1bca649e6d9e00
domin <dominikieltyka@wp.pl>
2017-06-12 17:07:08 +0200

    pattern

---
+++ src/main/java/com/kieltyka/simulator/Main.java
@@ -0,0 +1,10 @@
+package com.kieltyka.simulator;
+
+/**
+ * Created by domin on 6/12/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
